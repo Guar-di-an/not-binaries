fa05cb9846dff5c8605d84367ed1c7a07325b2bf
Guardian <84091675+Guar-di-an@users.noreply.github.com>
2021-05-13 01:23:17 +0600

    Add files via upload

---
+++ Unconventional Matrix.bat
@@ -0,0 +1,12 @@
+@echo off
+color 0a
+mode 1000
+ 
+:a
+ 
+echo %random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%
+ 
+ping > nul
+ping > nul
+ 
+goto a
